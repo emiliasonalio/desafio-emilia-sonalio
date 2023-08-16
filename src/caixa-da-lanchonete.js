e8f64003fa70b3a8d058d75f158767273517ab4f
emiliasonalio <emiliasonalio@gmail.com>
2023-08-16 17:18:50 -0300

    Mudanças 16/08

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,108 @@
+import { Item } from "./Item.js";
+import { Tabela } from "./tabela.js";
+import { ListaDeCompras } from "./lista-de-compras.js";
+
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        let valorDaCompra = 0;
+        let output = '';
+        try{
+            if(itens != ''){
+                try{
+                    if(metodoDePagamento != ''){
+                        let valor = 0;
+                        let tabela = new Tabela(); //instanciei o objeto tabela usando a classe Tabela
+                        let lista = new ListaDeCompras();
+                        let status;
+                
+                        for (let i = 0 ; i < itens.length; i++){
+                
+                            let pedido = itens[i].split(','); //dividindo a string na vírgula
+                            let index;
+                            try{
+                                index = tabela.acharCorrespondente(pedido[0]); // procurando a posição do pedido
+                                if(index == -1){
+                                    throw new Error ("Item inválido!");
+                                }
+                            }
+                            catch(e){
+                                output = "Item inválido!"
+                            }
+                            
+                            try{//tenta executa o código, se não conseguir joga o erro para o catch mais próximo
+                                if(pedido[1] != 0){
+                                    for (let j = 0 ; j < pedido[1]; j++){ //procurar a string pedido[0] na classe tabela
+                                        let item = new Item(tabela.codigo[index], tabela.preco[index], tabela.descricao[index]); // criando o objeto
+                                        lista.compras.push(item);
+                                    }
+                    
+                                }
+                                
+                                else{
+                                    throw new Error("Quantidade inválida!"); //lança o erro, procura o catch mais perto e capta a mensagem de erro  
+                                }
+                            }
+                            catch(e){
+                                output = "Quantidade inválida!"; //encerra o programa
+                            }
+                
+                        }
+                        try{
+                            status = lista.verificarLista();
+                            if (status != 'Sucesso!'){
+                                throw new Error (status);
+                            }
+                        }   
+                        catch(e){
+                            output = 'Item extra não pode ser pedido';
+                        }
+
+                        lista.compras.forEach(item => {
+                            valor += item.preco;
+                        });
+                        
+                        try{
+                            if(metodoDePagamento == 'dinheiro'){
+                                valorDaCompra = valor * 0.95;
+                            }
+                            else if(metodoDePagamento == 'credito'){
+                                valorDaCompra = valor * 1.03;
+                            }
+                            else if(metodoDePagamento == 'debito'){
+                                valorDaCompra = valor;
+                            }
+                            else{ 
+                                throw new Error ('Forma de pagamento inválida!');
+                            }
+        
+                            valorDaCompra = valorDaCompra.toFixed(2);
+                            output = 'R$ ' + valorDaCompra;
+                            output = output.replace(".",",")
+                        }
+                        catch(e){
+                            output = 'Forma de pagamento inválida!';
+                        }            
+                    }
+                    else{
+                        throw new Error ('Forma de pagamento inválida!');
+                    }
+                }
+                catch(e){
+                    output = 'Forma de pagamento inválida!';
+                }
+            }  
+            else{
+                throw new Error ('Não há itens no carrinho de compra!');
+            }
+        }
+        catch(e){
+            output = 'Não há itens no carrinho de compra!';
+        }
+        
+        
+        return output;
+    }
+
+}
+export { CaixaDaLanchonete };
--- src/tabela.js
+++ src/tabela.js
@@ -1,20 +1,22 @@
 class Tabela {
-    constructor(){
+    constructor(){//define a sequencia que os atributos são guardados
         this.codigo = ['cafe', 'chantily', 'suco', 'sanduiche', 'queijo', 'salgado', 'combo1', 'combo2'];
         this.preco = [3.00, 1.50, 6.20, 6.50, 2.00, 7.25, 9.50, 7.50];
         this.descricao = ['Café', 'Chantily', 'Suco Natural', 'Sanduíche', 'Queijo (Extra do Sanduíche', 'Salgado', '1 Suco e 1 Sanduíche', '1 Café e 1 Sanduíche'];
+
     }
 
     acharCorrespondente(pedido){
-        for(let i = 0; i < this.codigo.length, i++){
-            if (pedido = this.codigo[i]){
-                let posicao = i;
+        let posicao = -1;
+        for(let i = 0; i < this.codigo.length; i++){
+            if (pedido == this.codigo[i]){
+                posicao = i;
             }
-            
         }
-        
+
         return posicao;
     }
+
 }
 
 export { Tabela };
